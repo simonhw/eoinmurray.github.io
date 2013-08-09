56aace22777f2ab1b24ef352042ae05989e58387
amonecho <amonecho1@gmail.com>
2013-08-09 11:26:48 +0100

    Site updated at 2013-08-09 10:26:48 UTC

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,11 +1,13 @@
 $(function(){
 	var $bg = $('.bg-image')
     var yPos = -($(window).scrollTop()); 
-    var coords = yPos - 30 + 'px';
-    $bg.css({ backgroundPosition: coords });
+    var coords = yPos - 70 + 'px';
+    $bg.css('backgroundPosition', '0 ' + coords );
+    
     $(window).scroll(function() {
         var yPos = -($(window).scrollTop()/2.); 
-        var coords = yPos - 30 + 'px';
-        $bg.css({ backgroundPosition: coords });
+        var coords = yPos - 70 + 'px';
+        $bg.css('backgroundPosition', '0 ' + coords );
     }); 
+
 })
